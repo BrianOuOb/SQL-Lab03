6b791a4db4ef40256609e8e330765292528629bc
BrianOuOb <113296783+BrianOuOb@users.noreply.github.com>
2023-10-02 20:20:05 +0800

    Update 3-2-6ex02.sql

--- 3-2-6ex02.sql
+++ 3-2-6ex02.sql
@@ -1,2 +1,7 @@
 INSERT INTO Comp_dep
-VALUES ('D003', '��o��')
+VALUES ('D003', '¬ãµo³¡')
+
+
+訊息 2627，層級 14，狀態 1，行 1
+違反 PRIMARY KEY 條件約束 'PK__Comp_dep__34838F4C647203B1'。無法在物件 'dbo.Comp_dep' 中插入重複的索引鍵。重複的索引鍵值是 (D003)。
+陳述式已經結束。
